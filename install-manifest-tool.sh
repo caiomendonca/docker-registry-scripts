1d808eda80d469d0ff4d848ed3a3f7fcd78bdffd
Caio Mendonça <caiojose3@hotmail.com>
2023-10-01 18:08:09 -0300

    create install-manifest

---
+++ install-manifest-tool.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set variables
+MANIFEST_TOOL_VERSION=v2.0.8
+
+# install dependencies
+sudo apt install wget -y
+
+# download manifest-tool
+sudo wget -O /tmp/manifest-tool.tar.gz github.com/estesp/manifest-tool/releases/download/${MANIFEST_TOOL_VERSION}/binaries-manifest-tool-${MANIFEST_TOOL_VERSION}.tar.gz
+sudo mkdir -p /tmp/manifest-tool
+sudo ar -xf /tmp/manifest-tool.tar.gz --directory /tmp/manifest-tool
+
+# copy amd64 bin to /bin
+sudo mv /tmp/manifest-tool/manifest-tool-linux-amd64 /bin/manifest-tool
+
+# delete downloaded files
+sudo rm -rf /tmp/manifest-tool.tar.gz /tmp/manifest-tool
+
+# copy temmplates to /etc/manifest-tool
+sudo mkdir -p /etc/manifest-tool
+sudo cp $PWD/templates /etc/manifest-tool/
+
+
